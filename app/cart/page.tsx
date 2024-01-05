97f9aee167164a80b768e764b897c853b8deb4fa
Ifratbinte <ifratbinte@gmail.com>
2024-01-05 12:59:06 +0600

    feat[*]: increment decrement functionality added

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import React from "react";
-import { remove } from "@/store/CartSlice";
+import {
+  decrementQuantity,
+  incrementQuantity,
+  remove,
+} from "@/store/CartSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { FaTrashAlt } from "react-icons/fa";
+import { FaTimes, FaTrashAlt } from "react-icons/fa";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -20,13 +24,28 @@ const Cart = () => {
           <div className="flex items-center justify-between mb-6 py-3 px-6 rounded bg-slate-50">
             <img src={item.image} alt="img" className="min-h-16 max-h-16" />
             <h5 className="w-1/3">{item.title}</h5>
-            <h5 className="w-1/6 font-medium">${item.price}</h5>
+            <h5 className="w-1/6 font-medium flex items-center gap-2">
+              ${item.price} <FaTimes className="text-xs" /> {cartItems.length}
+            </h5>
             <div className="w-1/4 flex gap-4">
-              <button className="text-lg p-1 bg-slate-100 h-6 w-6 flex items-center justify-center border border-slate-200 cursor-pointer">+</button>
-              <button className="text-lg p-1 bg-slate-100 h-6 w-6 flex items-center justify-center border border-slate-200 cursor-pointer">-</button>
+              <button
+                className="text-lg p-1 bg-slate-100 h-6 w-6 flex items-center justify-center border border-slate-200 cursor-pointer"
+                onClick={() => dispatch(incrementQuantity())}
+              >
+                +
+              </button>
+              <button
+                className="text-lg p-1 bg-slate-100 h-6 w-6 flex items-center justify-center border border-slate-200 cursor-pointer"
+                onClick={() => dispatch(decrementQuantity())}
+              >
+                -
+              </button>
             </div>
-            <button className="text-red-500 hover:bg-red-100 p-2 rounded " onClick={() => handleRemove(item.id)}>
-              <FaTrashAlt/>
+            <button
+              className="text-red-500 hover:bg-red-100 p-2 rounded "
+              onClick={() => handleRemove(item.id)}
+            >
+              <FaTrashAlt />
             </button>
           </div>
         ))}
--- store/CartSlice.js
+++ store/CartSlice.js
@@ -1,17 +1,44 @@
 "use client";
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
-    name:"Cart",
-    initialState:[],
-    reducers:{
-        add(state,action){
-            state.push(action.payload)
-        },
-        remove(state,action){
-            return state.filter((item)=>item.id !== action.payload);
-        }
-    }
-})
+  name: "Cart",
+  initialState: [],
+  reducers: {
+    add(state, action) {
+      state.push(action.payload);
+    },
+    remove(state, action) {
+      return state.filter((item) => item.id !== action.payload);
+    },
+    // incrementQuantity: (state, action) => {
+    //   const item = state.find((item) => item.id === action.payload.id);
+    //   if (item) {
+    //     item.quantity += 1;
+    //   }
+    // },
+    // decrementQuantity: (state, action) => {
+    //   const item = state.find((item) => item.id === action.payload.id);
+    //   if (item && item.quantity > 1) {
+    //     item.quantity -= 1;
+    //   }
+    // },
+    incrementQuantity: (state) => {
+      state.length += 1;
+    },
+    decrementQuantity: (state) => {
+      state.length -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
 
-export const {add,remove} =cartSlice.actions;
+export const {
+  add,
+  remove,
+  incrementQuantity,
+  decrementQuantity,
+  incrementByAmount,
+} = cartSlice.actions;
 export default cartSlice.reducer;
