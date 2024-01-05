62ea0f28357de8d5ee89fb135d6e65693f5743e6
Ifratbinte <ifratbinte@gmail.com>
2024-01-05 12:12:33 +0600

    feat: markup for increment decrement

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { remove } from "@/store/CartSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -16,12 +17,16 @@ const Cart = () => {
       <div className="text-2xl font-medium text-center my-6">Cart page</div>
       <div className="w-2/3 mx-auto">
         {cartItems.map((item: any) => (
-          <div className="flex items-center justify-between mb-6 p-3 rounded bg-slate-50">
+          <div className="flex items-center justify-between mb-6 py-3 px-6 rounded bg-slate-50">
             <img src={item.image} alt="img" className="min-h-16 max-h-16" />
-            <h5 className="w-1/4">{item.title}</h5>
-            <h5 className="w-1/4">{item.price}</h5>
-            <button className="btn" onClick={() => handleRemove(item.id)}>
-              Remove
+            <h5 className="w-1/3">{item.title}</h5>
+            <h5 className="w-1/6 font-medium">${item.price}</h5>
+            <div className="w-1/4 flex gap-4">
+              <button className="text-lg p-1 bg-slate-100 h-6 w-6 flex items-center justify-center border border-slate-200 cursor-pointer">+</button>
+              <button className="text-lg p-1 bg-slate-100 h-6 w-6 flex items-center justify-center border border-slate-200 cursor-pointer">-</button>
+            </div>
+            <button className="text-red-500 hover:bg-red-100 p-2 rounded " onClick={() => handleRemove(item.id)}>
+              <FaTrashAlt/>
             </button>
           </div>
         ))}
